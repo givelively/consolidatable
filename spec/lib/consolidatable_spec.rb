41d0ae420bae15970eda22c06a88c529e6f1a1f9
Tim Lawrenz <697600+timlawrenz@users.noreply.github.com>
2022-07-19 18:14:19 -0400

    adding a spec

--- spec/lib/consolidatable_spec.rb
+++ spec/lib/consolidatable_spec.rb
@@ -1,9 +1,1 @@
 # frozen_string_literal: true
-
-describe Consolidatable do
-  it 'is possible to provide config options' do
-    described_class.config do |c|
-      expect(c).to eq(described_class)
-    end
-  end
-end
