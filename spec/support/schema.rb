fd575f0bc0b043e638d22e1819be8f0d4a818614
Tim Lawrenz <697600+timlawrenz@users.noreply.github.com>
2022-07-20 12:44:01 -0400

    adding a schema for testing

--- spec/support/schema.rb
+++ spec/support/schema.rb
@@ -1,17 +1,21 @@
+# frozen_string_literal: true
+
 ActiveRecord::Schema.define do
   self.verbose = false
 
-  create_table "consolidations", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "consolidatable_id", null: false
-    t.string "consolidatable_type", null: false
-    t.string "var_name", null: false
-    t.string "var_type", null: false
-    t.float "float_value"
-    t.integer "integer_value"
-    t.boolean "boolean_value"
-    t.string "string_value"
-    t.datetime "datetime_value"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'consolidations', id: :uuid, default: lambda {
+                                                       'gen_random_uuid()'
+                                                     }, force: :cascade do |t|
+    t.uuid 'consolidatable_id', null: false
+    t.string 'consolidatable_type', null: false
+    t.string 'var_name', null: false
+    t.string 'var_type', null: false
+    t.float 'float_value'
+    t.integer 'integer_value'
+    t.boolean 'boolean_value'
+    t.string 'string_value'
+    t.datetime 'datetime_value'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 end
