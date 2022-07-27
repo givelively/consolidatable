814d5b3f617904ed563f75a11741569730f061e0
Tim Lawrenz <697600+timlawrenz@users.noreply.github.com>
2022-07-27 17:12:41 -0400

    remove test method from class

--- lib/consolidatable/consolidation.rb
+++ lib/consolidatable/consolidation.rb
@@ -9,19 +9,6 @@ module Consolidatable
             where(var_name: variable.name, var_type: variable.type)
           }
 
-    def self.consolidate_them_all
-      Consolidation
-        .all
-        .distinct
-        .pluck(:consolidatable_type)
-        .each do |klass|
-          glass = klass.constantize
-          glass
-            .send(:class_variable_get, '@@consolidate_methods')
-            .each { |m| glass.find_each { |g| g.send(m) } }
-        end
-    end
-
     def self.detect(collection, variable)
       collection.detect do |c|
         c.var_name == variable.name && c.var_type.to_sym == variable.type
