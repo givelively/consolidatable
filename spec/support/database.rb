6886db50610b852229633283b5f209947b9bc496
Tim Lawrenz <697600+timlawrenz@users.noreply.github.com>
2025-03-10 22:12:03 -0400

    fixing database settings

--- spec/support/database.rb
+++ spec/support/database.rb
@@ -2,17 +2,30 @@
 
 require 'active_record'
 
-def db_config
-  {
-    adapter: 'postgresql',
-    host: ENV.fetch('POSTGRES_HOST', 'localhost'),
-    database: ENV.fetch('POSTGRES_DB', 'consolidatable_test'),
-    username: ENV.fetch('POSTGRES_USER', 'postgres'),
-    password: ENV.fetch('POSTGRES_PASSWORD', 'postgres'),
-    encoding: 'unicode',
-    pool: ENV.fetch('POSTGRES_POOL', 5).to_i,
-    min_messages: 'warning'
+db_config = {
+  'test' => {
+    'adapter' => 'postgresql',
+    'encoding' => 'unicode',
+    'host' => ENV.fetch('POSTGRES_HOST', 'localhost'),
+    'database' => ENV.fetch('POSTGRES_DB', 'consolidatable_test'),
+    'username' => ENV.fetch('POSTGRES_USER', 'postgres'),
+    'password' => ENV.fetch('POSTGRES_PASSWORD', 'postgres'),
+    'pool' => ENV.fetch('POSTGRES_POOL', 5).to_i,
+    'min_messages' => 'warning'
   }
-end
+}
 
-ActiveRecord::Base.establish_connection(db_config)
+ActiveRecord::Base.belongs_to_required_by_default = true if ActiveRecord.version.version >= '5'
+ActiveRecord::Base.establish_connection(db_config['test'])
+
+# Load and apply the schema
+load File.expand_path('../database/schema.rb', __dir__)
+
+RSpec.configure do |config|
+  config.before(:suite) do 
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean
+  end
+
+  config.before { DatabaseCleaner.clean }
+end
